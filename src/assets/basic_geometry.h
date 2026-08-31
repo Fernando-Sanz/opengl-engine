5ee73a64838b40394c4d7f0b34a1ec3aa74b41b6
Fernando-Sanz <76251393+Fernando-Sanz@users.noreply.github.com>
2026-08-31 02:53:37 +0200

    Add VBO, VAO and shaders (First triangle)

---
+++ src/assets/basic_geometry.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+namespace assets {
+
+	constexpr float triangleVertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include "main.h"
 
 #include "platform/Window.h"
+#include "assets/basic_geometry.h"
+#include "assets/vertex_shader.h"
+#include "assets/fragment_shader.h"
 
 
 constexpr int WINDOW_WIDTH = 800;
@@ -12,6 +15,10 @@ constexpr float BACKGROUND_COLOR[] = {
 };
 
 
+void configureVertexData(GLuint& vertexArray, GLuint& vertexBuffer, GLsizeiptr size, const void* data);
+GLuint createShaderProgram(const char* vertexshaderCode, const char* fragmentshaderCode);
+void checkShaderCompilation(GLuint shader);
+void checkShaderProgramLinking(GLuint shaderProgram);
 void processInput(platform::Window);
 void render();
 
@@ -21,6 +28,15 @@ int main()
     // INIT PLATFORM LIB AND CREATE A WINDOW
     platform::initPlatformLib();
     platform::Window window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+    // VBO AND VAO CONFIGURATION
+    GLuint vertexArray;
+    GLuint vertexBuffer;
+    configureVertexData(vertexArray, vertexBuffer, sizeof(assets::triangleVertices), assets::triangleVertices);
+
+    // SHADER PROGRAM
+    unsigned int shaderProgram = createShaderProgram(assets::vertexShaderSource, assets::fragmentShaderSource);
+    glUseProgram(shaderProgram);
 
     // RENDER LOOP
     while (!window.shouldClose())
@@ -40,12 +56,77 @@ int main()
     return 0;
 }
 
+
+void configureVertexData(GLuint& vertexArray, GLuint& vertexBuffer, GLsizeiptr size, const void* data) {
+    // VERTEX ARRAY OBJECT
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    // VERTEX BUFFER
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+
+    // VERTEX ATTRIBUTES DESCRIPTION
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+GLuint createShaderProgram(const char* vertexShaderCode, const char* fragmentShaderCode) {
+    // CREATE AND COMPILE SHADERS
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompilation(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompilation(fragmentShader);
+
+    // CREATE PROGRAM
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkShaderProgramLinking(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+void checkShaderCompilation(GLuint shader) {
+    int success;
+    char infolog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infolog);
+        std::cout << "ERROR: SHADER COMPILATION FAILED:\n" << infolog << std::endl;
+    }
+}
+
+void checkShaderProgramLinking(GLuint shaderProgram) {
+    int success;
+    char infolog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
+        std::cout << "ERROR: SHADER PROGRAM LINKING FAILED:\n" << infolog << std::endl;
+    }
+}
+
 void processInput(platform::Window window) {
     if (platform::getKey(window.getHandle(), GLFW_KEY_ESCAPE))
         window.closeWindow();
 }
 
 void render() {
+    // BACKGROUND
     glClearColor(
         BACKGROUND_COLOR[0],
         BACKGROUND_COLOR[1],
@@ -53,4 +134,7 @@ void render() {
         BACKGROUND_COLOR[3]
     );
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // RENDER
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
